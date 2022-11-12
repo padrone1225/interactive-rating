9db1488d22bfc2e9b1b6eb601e9329180b095c91
padrone1225 <lidinfomaborisovich@gmail.com>
2022-11-12 19:18:02 +0900

    star

---
+++ src/components/InteractiveRating.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
+export const InteractiveRating = () => {
+  return (
+    <div className="h-screen bg-Black-Blue">
+      <div className="p-8 rounded-3xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-Dark-Blue absolute">
+        <div id="star" className="rounded-full p-4 bg-Light-Grey">
+          <img src="/image/icon.svg" alt="Star" />
+        </div>
+        AAA
+      </div>
+    </div>
+  );
+};
